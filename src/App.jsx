128d82664035abcd03c564e217d4798c49a777eb
julka <julia.bromowicz@gmail.com>
2024-04-01 23:23:40 +0200

    front

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import React, {useState, useEffect} from "react";
 export const App = () => {
 
     const [value, setValue] = useState('')
-    const [responseData, setResponseData] = useState('')
+    const [responseData, setResponseData] = useState([])
 
-    useEffect(async () => {
-        const response = await fetch('http://localhost:8008/albums')
-        const data = await response.json()
-        setResponseData(data)
-    })
+    useEffect( () => {
+        const getData = async () => {
+            const response = await fetch('http://localhost:8008/albums')
+            const data = await response.json()
+            const datafilter = data.filter((x) => {
+                return (
+                    value && x.title.includes(value)
+                )   
+            })
+            setResponseData(datafilter)
+        }
+        getData()
+    }, [value])
 
     return (
         <div>
             <input value={value} onChange={(e) => setValue(e.target.value)}></input>
-            <div>{responseData}</div>
+            <div>{responseData.map((result) => {
+                return <div>{result.title}</div>
+            })}</div>
         </div>
     )
 }
